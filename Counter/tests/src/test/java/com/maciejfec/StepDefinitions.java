5eb79584d06ddfc76a23f5a45123fe1da7c5d970
Maciej Fec <maciejfec1996@gmail.com>
2022-08-03 19:21:02 +0100

    added a set up and tear down methods

--- Counter/tests/src/test/java/com/maciejfec/StepDefinitions.java
+++ Counter/tests/src/test/java/com/maciejfec/StepDefinitions.java
@@ -1,12 +1,34 @@
 package com.maciejfec;
 
+import com.maciejfec.baseclass.BaseClass;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import org.junit.jupiter.api.Assertions.*;
 
-public class StepDefinitions {
+public class StepDefinitions extends BaseClass {
+
+    @Before
+    public void setUp() {
+
+        super.setUp();
+        // Goes to specified page
+        super.getDriver().get(super.getBaseUrl());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @After
+    public void tearDown() {
+
+        super.tearDown();
+    }
 
     @Given("I am on the index page")
     public void i_am_on_the_index_page() {
