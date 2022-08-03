a1efa769f60810d2a892f9db6c68e25ed4501f75
Maciej Fec <maciejfec1996@gmail.com>
2022-08-03 19:00:29 +0100

    wrote step definitions

--- Counter/tests/src/test/java/com/maciejfec/StepDefinitions.java
+++ Counter/tests/src/test/java/com/maciejfec/StepDefinitions.java
@@ -8,4 +8,31 @@ import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
+    @Given("I am on the index page")
+    public void i_am_on_the_index_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click the {string} button")
+    public void i_click_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The number is increased")
+    public void the_number_is_increased() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The number is decreased")
+    public void the_number_is_decreased() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The number should be {int}")
+    public void the_number_should_be(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
