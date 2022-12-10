2e877fb0040d30d194a602d488c3426cc745bf44
Maciej Fec <maciejfec1996@gmail.com>
2022-12-10 21:51:40 +0000

    Add a dummy list of reviews

---
+++ ReviewCarousel/script.js
@@ -0,0 +1,50 @@
+const reviews = [
+    {
+        id: 1,
+        photoPath: "/",
+        fullName: "Rosa Florian",
+        review: ""
+    },
+    {
+        id: 2,
+        photoPath: "/",
+        fullName: "Christoffer Laelia",
+        review: "Literature admiration frequently indulgence announcing are who you her. Was least quick after six. So it yourself repeated together cheerful. Neither it cordial so painful picture studied if. Sex him position doubtful resolved boy expenses. Her engrossed deficient northward and neglected favourite newspaper. But use peculiar produced concerns ten."
+    },
+    {
+        id: 3,
+        photoPath: "/",
+        fullName: "Daniel Kamiyah",
+        review: "In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old."
+    },
+    {
+        id: 4,
+        photoPath: "/",
+        fullName: "Amaia Sofia",
+        review: "Kept in sent gave feel will oh it we. Has pleasure procured men laughing shutters nay. Old insipidity motionless continuing law shy partiality. Depending acuteness dependent eat use dejection. Unpleasing astonished discovered not nor shy. Morning hearted now met yet beloved evening. Has and upon his last here must."
+    },
+    {
+        id: 5,
+        photoPath: "/",
+        fullName: "Lucilius Yarognev",
+        review: "Admiration stimulated cultivated reasonable be projection possession of. Real no near room ye bred sake if some. Is arranging furnished knowledge agreeable so. Fanny as smile up small. It vulgar chatty simple months turned oh at change of. Astonished set expression solicitude way admiration."
+    },
+    {
+        id: 6,
+        photoPath: "/",
+        fullName: "Erik Parthenope",
+        review: "It as announcing it me stimulated frequently continuing. Least their she you now above going stand forth. He pretty future afraid should genius spirit on. Set property addition building put likewise get. Of will at sell well at as. Too want but tall nay like old. Removing yourself be in answered he. Consider occasion get improved him she eat. Letter by lively oh denote an."
+    },
+    {
+        id: 7,
+        photoPath: "/",
+        fullName: "Adriana Klara",
+        review: ""
+    },
+    {
+        id: 8,
+        photoPath: "/",
+        fullName: "Anabelle Kambyses",
+        review: "Luckily friends do ashamed to do suppose. Tried meant mr smile so. Exquisite behaviour as to middleton perfectly. Chicken no wishing waiting am. Say concerns dwelling graceful six humoured. Whether mr up savings talking an. Active mutual nor father mother exeter change six did all."
+    }
+]
